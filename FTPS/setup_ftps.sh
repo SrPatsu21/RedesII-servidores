ecf82e2c94a5f57578f8ce2286d37cab39da1dce
SrPatu21 <paulosergiopierdona@gmail.com>
2025-11-27 19:34:44 -0300

    prev

--- FTPS/setup_ftps.sh
+++ FTPS/setup_ftps.sh
@@ -1,67 +1,49 @@
-#!/bin/bash
-
+#!/bin/sh
 set -e
 
-source .env
+# Valores estáticos dentro do container
+FTP_PORT=21
+PASV_MIN=40000
+PASV_MAX=40100
 
-# Expected environment variables:
-# IP_SERVIDOR="x.x.x.x"
-# FTP_USER="alunoftp"
-# FTP_PASS="senha123"
-# FTP_PORT="21001"
+IP_SERVIDOR="${IP_SERVIDOR:-192.168.0.100}"
+FTP_USER="${FTP_USER:-"alunoftp"}"
+FTP_PASS="${FTP_PASS:-"senha123"}"
 
-if [ -z "$IP_SERVIDOR" ] || [ -z "$FTP_USER" ] || [ -z "$FTP_PASS" ] || [ -z "$FTP_PORT" ]; then
-  echo "ERROR: Missing environment variables."
-  echo "Required: IP_SERVIDOR, FTP_USER, FTP_PASS, FTP_PORT"
-  exit 1
-fi
-
-echo "[1/8] Updating packages..."
-apt update -y
-
-echo "[2/8] Installing vsftpd..."
-apt install vsftpd -y
-
-echo "[3/8] Creating SSL directory..."
+echo "[1/6] Creating SSL directory..."
 mkdir -p /etc/ssl/private
 chmod 700 /etc/ssl/private
 
-echo "[4/8] Generating SSL certificate..."
+echo "[2/6] Generating SSL certificate..."
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/ssl/private/vsftpd.pem \
   -out /etc/ssl/certs/vsftpd.pem \
   -subj "/C=BR/ST=SC/L=Videira/O=AngelCorp/OU=IT/CN=ftp.angelcorp.com.br"
 
-echo "[5/8] Writing /etc/vsftpd.conf..."
-
-cat <<EOF > /etc/vsftpd.conf
+echo "[3/6] Writing /etc/vsftpd.conf..."
+cat > /etc/vsftpd.conf <<EOF
 listen=YES
 listen_ipv6=NO
-
-listen_port=${PORTA}
+listen_port=${FTP_PORT}
 
 anonymous_enable=NO
 local_enable=YES
 write_enable=YES
-
 dirmessage_enable=YES
 use_localtime=YES
 xferlog_enable=YES
 connect_from_port_20=YES
 
 secure_chroot_dir=/var/run/vsftpd/empty
-pam_service_name=vsftpd
 
 # SSL
 ssl_enable=YES
 rsa_cert_file=/etc/ssl/certs/vsftpd.pem
 rsa_private_key_file=/etc/ssl/private/vsftpd.pem
-
 ssl_tlsv1_2=YES
 ssl_tlsv1_3=YES
 ssl_sslv2=NO
 ssl_sslv3=NO
-
 force_local_data_ssl=YES
 force_local_logins_ssl=YES
 
@@ -75,25 +57,21 @@ chroot_local_user=YES
 allow_writeable_chroot=YES
 EOF
 
-echo "[6/8] Creating FTP user..."
-useradd -m -s /bin/bash "$FTP_USER"
-echo "$FTP_USER:$FTP_PASS" | chpasswd
-chmod 755 /home/$FTP_USER
+echo "[4/6] Creating FTP user..."
+adduser -D "$FTP_USER"
+echo "${FTP_USER}:${FTP_PASS}" | chpasswd
+chmod 755 "/home/${FTP_USER}"
 
-echo "[7/8] Restarting vsftpd..."
-systemctl restart vsftpd
-systemctl enable vsftpd
+echo "[5/6] Ensuring vsftpd runtime dir..."
+mkdir -p /var/run/vsftpd/empty
 
-echo "[8/8] Installing lftp (optional for testing)..."
-apt install lftp -y
-
+echo "[6/6] FTPS base configuration done. vsftpd will be started by the container command."
 echo
 echo "=============================================="
-echo " FTPS successfully configured!"
-echo " FTP User:     $FTP_USER"
+echo " FTPS configured on Alpine!"
+echo " FTP User: $FTP_USER"
 echo " FTP Password: $FTP_PASS"
-echo " Server IP:    $IP_SERVIDOR"
-echo " Control Port: $FTP_PORT"
-echo
-echo " Use FileZilla or lftp to connect using FTPS."
+echo " Server IP: $IP_SERVIDOR"
+echo " Control Port (internal): $FTP_PORT"
+echo " Passive Ports (internal): ${PASV_MIN}-${PASV_MAX}"
 echo "=============================================="
