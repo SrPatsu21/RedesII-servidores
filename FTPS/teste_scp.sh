ad26e0ed44fffd3278e65345e4d713d6108834af
SrPatu21 <paulosergiopierdona@gmail.com>
2025-11-27 22:45:12 -0300

    m

--- FTPS/teste_scp.sh
+++ FTPS/teste_scp.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-source .env
+source ../.env
 
 if [ -z "$ARQUIVO" ]; then
     echo "Uso: ./enviar.sh arquivo.txt"
     exit 1
 fi
 
-echo "Enviando '$ARQUIVO' para $FTP_USER@$IP_SERVIDOR:$DESTINO ..."
-scp -P "$FTP_PORT" "$ARQUIVO" "$FTP_USER@$IP_SERVIDOR:$DESTINO"
+echo "Enviando '$ARQUIVO' para $FTP_USER@$IP_SERVIDOR:$DESTINATION ..."
+scp -P "$FTP_PORT" "$ARQUIVO" "$FTP_USER@$IP_SERVIDOR:$DESTINATION"
 
 if [ $? -eq 0 ]; then
     echo "Arquivo enviado com sucesso!"
