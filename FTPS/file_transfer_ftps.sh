ca898dd55276f161d986ac5a80cc5e42cd380a10
SrPatu21 <paulosergiopierdona@gmail.com>
2025-11-28 21:29:44 -0300

    fix

--- FTPS/file_transfer_ftps.sh
+++ FTPS/file_transfer_ftps.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Configurações (ajuste estes valores)
-VM_USER="seu_usuario_vm"  # Usuário da VM
-VM_IP="10.0.0.5"          # IP da VM/servidor
-VM_SSH_PORT="22"          # Porta SSH mapeada (se não for 22)
-FTP_PORT="21"             # Porta FTP mapeada
+VM_USER="patsu21"
+VM_IP="10.0.0.5"          
+VM_SSH_PORT="22"
+FTP_PORT="21"
 FTP_USER="alunoftp"
 FTP_PASS="pass123"
 NGINX_DIR="/home/${VM_USER}/nginx_angelcorp/html/www"
 LOCAL_FILE="ftps_seguro.html"
 FTP_HOME="/home/${FTP_USER}"
 
-# 1. Criar arquivo local
+# 1. criate file
 echo "Este é um arquivo transferido via FTPS seguro para o Nginx!" > $LOCAL_FILE
-echo "Arquivo criado: $LOCAL_FILE"
+echo "file created: $LOCAL_FILE"
 
-# 2. Transferir via LFTP para diretório FTP
-lftp -p $FTP_PORT -u $FTP_USER,$FTP_PASS $VM_IP << EOF
+# 2. transfer file to server
+lftp -u $FTP_USER,$FTP_PASS -p $FTP_PORT $VM_IP << EOF
 set ftp:ssl-force true
 cd $FTP_HOME
 put $LOCAL_FILE
@@ -24,19 +23,10 @@ bye
 EOF
 
 if [ $? -eq 0 ]; then
-  echo "Transferência FTPS concluída."
+  echo "transference finished."
 else
-  echo "Erro na transferência FTPS."
+  echo "Error on FTPS transference."
   exit 1
-fi
-
-# 3. Mover na VM via SSH
-ssh -p $VM_SSH_PORT $VM_USER@$VM_IP "sudo mv $FTP_HOME/$LOCAL_FILE $NGINX_DIR/"
-if [ $? -eq 0 ]; then
-  echo "Arquivo movido para Nginx na VM."
-  echo "Verifique no navegador: http://www.angelcorp.com.br:<PORTA_HTTP>/$LOCAL_FILE"
-else
-  echo "Erro ao mover arquivo na VM."
 fi
 
 # Limpar local
